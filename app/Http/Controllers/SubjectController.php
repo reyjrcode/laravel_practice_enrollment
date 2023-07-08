ca36912e61b560e84643045959f4cbcc9bc1f8ab
reyjrcode <rey.alcalajr.code@gmail.com>
2023-07-08 11:38:21 +0800

    route list

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SubjectCollection;
+use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,8 @@ class SubjectController extends Controller
         // php artisan make:resource SubjectResource
         // php artisan make:resource SubjectCollection
         return new SubjectCollection(Subject::all());
+    }
+    public function show(Request $request,Subject $subject){
+        return new SubjectResource($subject);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('subjects',[SubjectController::class,'index']);
+Route::apiResource('subjects', SubjectController::class)->only(
+    [
+        'index',
+        'show'
+    ]
+);
