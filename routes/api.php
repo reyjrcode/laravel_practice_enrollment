ccd25810160814218b93332f694bf7d1c5ba67fe
reyjrcode <rey.alcalajr.code@gmail.com>
2023-07-08 14:51:19 +0800

    LOggin in with user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource(
-    'subjects', SubjectController::class);
+    'subjects', SubjectController::class
+);
